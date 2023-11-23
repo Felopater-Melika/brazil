848b80733cc72cecd00d9da1af701d3d1d52a608
Felopater Melika <felopatermelika@gmail.com>
2023-11-22 19:39:43 -0600

    industries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: ['@radix-ui/themes']
+    }
+  },
+  integrations: [react(), tailwind()]
+});
